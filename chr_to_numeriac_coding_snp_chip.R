b43d44115169f96ab6438b1e1d6184a521ea7ca3
abhi_g <66737333+AbhishekGogna@users.noreply.github.com>
2022-05-03 12:00:31 +0200

    Added function to get allele counts

--- chr_to_numeriac_coding_snp_chip.R
+++ chr_to_numeriac_coding_snp_chip.R
@@ -1,7 +1,42 @@
-c_data <- read.table("chr_to_numeriac_coding_snp_chip_1.txt", header = T)
-g_data <- read.table("chr_to_numeriac_coding_snp_chip_2.txt", header = T)
+g_data <- read.table("data/chr_to_numeriac_coding_snp_chip_2.txt", header = T)
+`%!in%` = Negate(`%in%`)
 
-recode_markers<-function(count_data, genodata, n_ind = 19){
+get_counts <- function(geno_data){
+  geno_mat <- as.matrix(geno_data)[, 2:ncol(geno_data)]
+  rownames(geno_mat) <- geno_data$Marker
+  
+  out_counts <- NULL
+  for(i in 1:nrow(geno_mat)){
+    counts <- table(geno_mat[i, ])
+    calls <- names(counts)
+    het_calls <- c("R", "Y", "S", "W", "K", "M")
+    if(sum(calls %in% het_calls) > 0){
+      print(paste0(rownames(geno_mat)[i], " marker has het calls"))
+      het <- calls[which(calls %in% het_calls)]
+    } else { het <- NA }
+    
+    counts_no_het <- counts[which(calls %!in% het_calls)]
+    calls_no_het <- names(counts_no_het)
+    
+    if(length(counts_no_het) == 2){
+      major_al <- calls_no_het[which(counts_no_het/ncol(geno_mat) > 0.5)]
+      minor_al <- calls_no_het[which(counts_no_het/ncol(geno_mat) < 0.5)]
+    } else if (length(counts) == 1){
+      print(paste0(rownames(geno_mat)[i], " marker is monomorphic"))
+      major_al <- calls_no_het[which(counts_no_het/ncol(geno_mat) > 0.5)]
+      minor_al <- NA
+    }
+    
+    out_counts <- rbind(out_counts, cbind("Marker" = rownames(geno_mat)[i], major_al, minor_al, het))
+  }
+  out_counts_df <- as.data.frame(out_counts)
+  return(out_counts_df)
+}
+
+recode_markers<-function(genodata, n_ind = 19){
+  
+  count_data <- get_counts(genodata)
+  
   output<-matrix(0 ,dim(count_data)[1], n_ind+1)
   colnames(genodata)[1] <- colnames(count_data)[1]<- "Marker"
   colnames(output)<-colnames(genodata)
@@ -22,4 +57,4 @@ recode_markers<-function(count_data, genodata, n_ind = 19){
   return(GNdata)
 }
 
-dummy <- recode_markers(c_data, g_data)
+dummy <- recode_markers(g_data)
